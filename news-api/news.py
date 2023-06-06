ebb08816596c902a5e2ead09c42f7ec5b476df41
incri <paudelbaibhav0@gmail.com>
2023-06-06 16:56:32 +0545

    test news data with unblocked Cross-Origin Resource Sharing

--- news-api/news.py
+++ news-api/news.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# CORS configuration
+origins = [
+    "http://localhost:5173",  # Replace with your React app's origin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 
 class Article(BaseModel):
