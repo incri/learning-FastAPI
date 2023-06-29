04d5cadc9fb908165f7f270d9137dd7e4c49590a
FraNzY <anoj1810@gmail.com>
2023-06-29 22:55:44 +0545

    learning FastAPI from scratch

--- Fastapi_learning/main.py
+++ Fastapi_learning/main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI  #importing fastapi 
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
-app = FastAPI()  # creating instance
+app = FastAPI()
 
-@app.get('/') #decorating to indicate function
-def index():    #creating function
-    return {'Data' : {'Name' :'Anoj'}}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = False,sort :Optional[str]=None):
+    # only get 10 published blogs by default
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
-@app.get('/about')
-def about():
-    return {'Data':'About Page'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments of blog with id
+    return {'data': {'1', '2'}}
+
+#creating the Blog model
+class Blog(BaseModel):
+    title : str
+    body : str
+    published : Optional[bool]
+       
+
+@app.post('/blog')
+#using blog model
+def create_blog(blog:Blog):
+    return {'data':f"Blog is created with title as {blog.title}"} #getting valuesd
