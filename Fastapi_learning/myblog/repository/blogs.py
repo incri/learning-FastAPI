9eb0d487d465225cd2665ffa6ad5e7ff15481c01
FraNzY <anoj1810@gmail.com>
2023-07-25 18:16:17 +0545

    Route behind Authentication

--- Fastapi_learning/myblog/repository/blogs.py
+++ Fastapi_learning/myblog/repository/blogs.py
@@ -1,6 +1,5 @@
 from fastapi import HTTPException , status
 from sqlalchemy.orm import Session
-# from ..import models ,schemas
 import models ,schemas
 
 
--- Fastapi_learning/myblog/routers/authentication.py
+++ Fastapi_learning/myblog/routers/authentication.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter ,Depends ,status ,HTTPException
-# from .. import schemas , database ,models ,JWTtoken
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-# from .. hashing import Hashing
 from hashing import Hashing
 import schemas , database ,models ,JWTtoken
+import sys
+
+
 
 router = APIRouter(
     tags=['Authentication']
 )
-
-
+print("___________________________________________________________________________________________")
+print(sys.path)
+print("___________________________________________________________________________________________")
 @router.post('/login')
-def login(authentication : schemas.Login ,db:Session = Depends(database.get_db)):
+def login(authentication : OAuth2PasswordRequestForm = Depends() ,db:Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == authentication.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
@@ -20,7 +23,6 @@ def login(authentication : schemas.Login ,db:Session = Depends(database.get_db))
            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Invalid Password")
    
-
     
     access_token = JWTtoken.create_access_token( data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
