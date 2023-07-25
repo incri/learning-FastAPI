e4f4d5d4f027e0d50f80e02eb55f2d8e523b21a5
incri <paudelbaibhav0@gmail.com>
2023-07-25 15:14:30 +0545

    Book detail endpoint using id as parameter

--- advance-fast-api/Practice-API-2/books.py
+++ advance-fast-api/Practice-API-2/books.py
@@ -1,7 +1,7 @@
-from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from uuid import UUID
+
 
 app = FastAPI()
 
@@ -13,13 +13,14 @@ class BOOK(BaseModel):
     description: str = Field(max_length=100, min_length=1)
     rating: int = Field(gt=-1, lt=6)
 
-    class config:
+    class Config:
         schema_extra = {
             "example": {
                 "id": "UUID",
-                "title": "Title OF Book",
-                "author": " Author's Name",
-                "description": " Description Of Book",
+                "title": "Title Of Book",
+                "author": "Author's Name",
+                "description": "Description Of Book",
+                "rating": 4,
             }
         }
 
@@ -30,6 +31,13 @@ BOOKS = []
 @app.get("/")
 async def books():
     return BOOKS
+
+
+@app.get("/book/{id}")
+async def book_detail(id: UUID):
+    for book in BOOKS:
+        if book.id == id:
+            return book
 
 
 @app.post("/")
