f60c2365f7f24e0512398d49b185bb6a56b07440
incri <paudelbaibhav0@gmail.com>
2023-07-25 12:33:20 +0545

    Enumerationpath parameters

--- advance-fast-api/Practice-API/books.py
+++ advance-fast-api/Practice-API/books.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,11 +12,30 @@ BOOKS = {
 }
 
 
+class DirectionName(str, Enum):
+    north = "North"
+    south = "south"
+    east = "East"
+    west = "west"
+
+
 @app.get("/")
 async def books():
     return BOOKS
 
 
 @app.get("/books/{id}")
-async def book_detail(id : int):
+async def book_detail(id: int):
     return {"id": id}
+
+
+@app.get("/directions/{direction_name}")
+async def direction_detail(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {"Direction": direction_name, "sub": "up"}
+    if direction_name == DirectionName.south:
+        return {"Direction": direction_name, "sub": "Down"}
+
+    if direction_name == DirectionName.west:
+        return {"Direction": direction_name, "sub": "Left"}
+    return {"Direction": direction_name, "sub": "Right"}
