ff88f44e21dacb5424003749570e2f301acb4061
incri <paudelbaibhav0@gmail.com>
2023-07-25 13:55:07 +0545

    adding crud function endpoints to books project

--- advance-fast-api/Practice-API/books.py
+++ advance-fast-api/Practice-API/books.py
@@ -5,11 +5,11 @@ from enum import Enum
 app = FastAPI()
 
 BOOKS = {
-    "book_1": {"id": 1, "title": "Title One", "author": "Author One"},
-    "book_2": {"id": 2, "title": "Title Two", "author": "Author Two"},
-    "book_3": {"id": 3, "title": "Title Three", "author": "Author Three"},
-    "book_4": {"id": 4, "title": "Title Four", "author": "Author Four"},
-    "book_5": {"id": 5, "title": "Title Five", "author": "Author Five"},
+    "book_1": {"title": "Title One", "author": "Author One"},
+    "book_2": {"title": "Title Two", "author": "Author Two"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"},
 }
 
 
@@ -25,3 +25,30 @@ async def books(skip_book: Optional[str] = None):
 @app.get("/{book_name}")
 async def book_detail(book_name: str):
     return BOOKS[book_name]
+
+
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
+
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            book_id = int(book.split("_")[-1])
+            if book_id > current_book_id:
+                current_book_id = book_id
+
+    BOOKS[f"book_{current_book_id + 1}"] = {"title": book_title, "author": book_author}
+    return BOOKS[f"book_{current_book_id + 1}"]
+
+
+@app.put("/{book_name}")
+async def update_book(book_name: str, book_title: str, book_author: str):
+    book_information = {"title": book_title, "author": book_author}
+    BOOKS[book_name] = book_information
+    return book_information
+
+
+@app.delete("/{book_name}")
+async def delete_book(book_name):
+    del BOOKS[book_name]
+    return f"Book_{book_name} was deleted"
