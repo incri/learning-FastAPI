72df25f05894ac164d8ca65d462c8e36d813886d
incri <paudelbaibhav0@gmail.com>
2023-07-26 12:23:09 +0545

    created User table and created connection between todos and User table

--- advance-fast-api/todo-app/models.py
+++ advance-fast-api/todo-app/models.py
@@ -1,14 +1,29 @@
-from sqlalchemy import Boolean, Integer, Column, String
+from sqlalchemy import Boolean, Integer, Column, String, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    task = relationship("Todos", back_populates="owner")
+
 
 class Todos(Base):
     __tablename__ = "todos"
 
-    id = Column(Integer, primary_key=True, index = True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
 
-    
- 
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("Users", back_populates="task")
