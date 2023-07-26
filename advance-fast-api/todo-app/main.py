688fc19683ecebd973b53870146ab3a3674bc506
incri <paudelbaibhav0@gmail.com>
2023-07-26 11:12:01 +0545

    task post endpoint added with pydantic BaseModel Validation

--- advance-fast-api/todo-app/main.py
+++ advance-fast-api/todo-app/main.py
@@ -1,7 +1,9 @@
+from typing import Optional
 from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -17,6 +19,13 @@ def get_db():
         db.close()
 
 
+class Task(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="Must be between 1 to 5")
+    complete: bool = Field(default=False)
+
+
 @app.get("/task/")
 async def task(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
@@ -28,6 +37,20 @@ async def task_detail(id: int, db: Session = Depends(get_db)):
     if model is not None:
         return model
     raise http_exception_404_not_found()
+
+
+@app.post("/task")
+async def create_task(task: Task, db: Session = Depends(get_db)):
+    model = models.Todos()
+    model.title = task.title
+    model.description = task.description
+    model.priority = task.priority
+    model.complete = task.complete
+
+    db.add(model)
+    db.commit()
+
+    return {"status": 201, "transaction": "Created Successfully"}
 
 
 def http_exception_404_not_found():
